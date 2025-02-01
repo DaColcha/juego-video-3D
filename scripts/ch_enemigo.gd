de828a306bb1db0827477ff414f4e273be373c74
Martin Mendieta <marmenthor@gmail.com>
2025-02-01 02:22:16 -0500

    Implementa mecánicas de juego al proyecto

---
+++ scripts/ch_enemigo.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+
+
+var angle=0
+func _ready():
+	set_physics_process(true)
+	pass
+	
+func _physics_process(delta):
+		
+	angle = angle + (PI*delta)
+	if angle >= 3*PI:
+			angle=0
+	set_rotation(Vector3(0, angle*2, 0))
